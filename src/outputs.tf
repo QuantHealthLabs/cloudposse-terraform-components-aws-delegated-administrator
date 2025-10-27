3c65175c7a4a244342569d120c5a9b70daf0af34
Sharon Dagan <sharon.dagan@quanthealth.ai>
2025-10-28 00:48:35 +0200

    update

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,7 @@
 output "delegations" {
-  value       = local.delegations
-  description = "A list of delegations"
+  description = "Map of service principals to account IDs"
+  value = {
+    for k, v in local.delegations :
+    v.principal => v.account_id
+  }
 }
